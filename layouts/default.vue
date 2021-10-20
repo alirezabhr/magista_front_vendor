dc8a8512c390a04d0654fad3426959cc7e82b4b7
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-21 00:33:53 +0330

    navigation drawer added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,51 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
+  <v-app>
+    <v-main class="background">
       <v-container>
-        <Nuxt />
+        <v-row class="pt-5">
+          <v-card class="hidden-sm-and-down" width="256">
+            <v-navigation-drawer class="surface accent-4" permanent>
+              <v-list-item class="primary" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="text-h5 my-3">
+                    مغازه
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list>
+                <v-list-item
+                  v-for="item in items"
+                  :key="item.title"
+                  link
+                  :to="item.navigation"
+                >
+                  <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title class="grey--text text--darken-8">
+                      {{ item.title }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+
+              <v-divider class="mx-2" />
+
+              <div class="pa-2">
+                <v-btn block class="primaryVariant2"> خروج از حساب </v-btn>
+              </div>
+            </v-navigation-drawer>
+          </v-card>
+
+          <v-col offset="2" md="6" class="surface rounded-lg mx-3">
+            <Nuxt />
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -92,25 +53,25 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: 'پیشخوان',
+          icon: 'mdi-view-dashboard-outline',
+          navigation: '/'
+        },
+        { title: 'سفارشات', icon: 'mdi-cart-variant', navigation: '/orders' },
+        { title: 'آمار', icon: 'mdi-chart-bar', navigation: '/stats' },
+        {
+          title: 'پیام‌ها',
+          icon: 'mdi-message-outline',
+          navigation: '/messages'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'تنظیمات',
+          icon: 'mdi-store-cog-outline',
+          navigation: '/settings'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
     }
   }
 }
