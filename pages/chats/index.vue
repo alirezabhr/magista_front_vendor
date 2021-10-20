d60840c3fceb6eab1cd110dc6eac66bc4a5f7c66
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-21 00:52:39 +0330

    page created

---
+++ pages/chats/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Chats page</div>
+</template>
+
+<script>
+export default {
+  name: 'Chats'
+}
+</script>
---
+++ pages/orders/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Orders page</div>
+</template>
+
+<script>
+export default {
+  name: 'Orders'
+}
+</script>
