92afeeb7b4424d619094b9bbfe83b01049980af1
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-21 13:36:22 +0330

    bottom navigation bar component created

---
+++ components/BottomNavigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-bottom-navigation
+    color="primary"
+    grow
+    fixed
+    class="hidden-md-and-up"
+    height="50px"
+  >
+    <v-btn
+      v-for="item in items"
+      :key="item"
+      :to="item.navigation"
+      height="50px"
+    >
+      <span>{{ item.title }}</span>
+
+      <v-icon>{{ item.icon }}</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: 'BottomNavigation',
+  data () {
+    return {
+      items: [
+        {
+          title: 'پیشخوان',
+          icon: 'mdi-view-dashboard-outline',
+          navigation: '/'
+        },
+        { title: 'سفارشات', icon: 'mdi-cart-variant', navigation: '/orders' },
+        { title: 'آمار', icon: 'mdi-chart-bar', navigation: '/stats' },
+        {
+          title: 'پیام‌ها',
+          icon: 'mdi-message-outline',
+          navigation: '/chats'
+        },
+        {
+          title: 'تنظیمات',
+          icon: 'mdi-store-cog-outline',
+          navigation: '/settings'
+        }
+      ]
+    }
+  }
+}
+</script>
