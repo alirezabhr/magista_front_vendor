b0d0abbd4fd979cef6920f35d1fc77aea8258e76
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-21 14:29:24 +0330

    auth middleware created to check user authentication

---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+export default function ({ store, redirect }) {
+  // eslint-disable-next-line no-console
+  console.log('[ Middleware ] auth is running')
+
+  // If the user is not authenticated
+  // if (!store.state.authenticated) {
+  //   return redirect('/login')
+  // }
+}
