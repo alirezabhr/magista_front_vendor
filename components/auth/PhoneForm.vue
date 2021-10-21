39d78ca181f00ae71b8c42617889103cceab179a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-21 16:21:04 +0330

    phone from component created for authentication

---
+++ components/auth/PhoneForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="pb-4">
+    <v-card-title>
+      <v-col>
+        <div class="text-h6 font-weight-bold">ثبت‌نام / ورود</div>
+        <div class="text-subtitle-2 font-weight-light pt-2">شماره موبایل خود را وارد کنید.</div>
+      </v-col>
+    </v-card-title>
+
+    <v-col class="py-0 px-8">
+      <div class="text-subtitle-2 grey--text">
+        مثال:
+        <span class="ltr">0912 ××× ×× ××</span>
+      </div>
+      <v-text-field
+        label="شماره موبایل"
+        outlined
+        dense
+        class="py-2"
+      />
+    </v-col>
+
+    <v-card-actions class="px-8">
+      <v-btn block class="primary font-weight-bold py-5" rounded>
+        تایید
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PhoneForm'
+}
+</script>
