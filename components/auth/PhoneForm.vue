45ef3ea1b5da0ae5670c309ea17b0d7fa4f96397
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-23 00:11:43 +0330

    rules added for phone input

--- components/auth/PhoneForm.vue
+++ components/auth/PhoneForm.vue
@@ -1,38 +1,45 @@
 <template>
   <v-card class="pb-4">
-    <v-card-title>
-      <v-col>
-        <div class="text-h6 font-weight-bold">ثبت‌نام / ورود</div>
-        <div class="text-subtitle-2 font-weight-light pt-2">شماره موبایل خود را وارد کنید.</div>
+    <v-form
+      ref="form"
+      lazy-validation
+    >
+      <v-card-title>
+        <v-col>
+          <div class="text-h6 font-weight-bold">ثبت‌نام / ورود</div>
+          <div class="text-subtitle-2 font-weight-light pt-2">شماره موبایل خود را وارد کنید.</div>
+        </v-col>
+      </v-card-title>
+
+      <v-col class="py-0 px-8">
+        <div class="text-subtitle-2 grey--text">
+          مثال:
+          <span class="ltr">0912 ××× ×× ××</span>
+        </div>
+        <v-text-field
+          v-model="phoneNo"
+          label="شماره موبایل"
+          :rules="rules"
+          outlined
+          dense
+          class="py-2"
+          type="number"
+          style="direction: ltr"
+        />
       </v-col>
-    </v-card-title>
 
-    <v-col class="py-0 px-8">
-      <div class="text-subtitle-2 grey--text">
-        مثال:
-        <span class="ltr">0912 ××× ×× ××</span>
-      </div>
-      <v-text-field
-        v-model="phoneNo"
-        label="شماره موبایل"
-        outlined
-        dense
-        class="py-2"
-        type="number"
-      />
-    </v-col>
-
-    <v-card-actions class="px-8">
-      <v-btn
-        block
-        rounded
-        class="primary font-weight-bold py-5"
-        :loading="isSubmittingPhone"
-        @click.prevent="$emit('submitForm', phoneNo)"
-      >
-        تایید
-      </v-btn>
-    </v-card-actions>
+      <v-card-actions class="px-8">
+        <v-btn
+          block
+          rounded
+          class="primary font-weight-bold py-5"
+          :loading="isSubmittingPhone"
+          @click.prevent="validateAndSubmitForm"
+        >
+          تایید
+        </v-btn>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
@@ -47,7 +54,19 @@ export default {
   },
   data () {
     return {
-      phoneNo: ''
+      phoneNo: '',
+      rules: [
+        value => !!value || '.شماره موبایل الزامی می‌باشد',
+        v => v.length === 11 || '.شماره موبایل دارای 11 رقم می‌باشد',
+        value => value.startsWith('09') || '.شماره موبایل باید با 09 شروع شود'
+      ]
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submitForm', this.phoneNo)
+      }
     }
   }
 }
