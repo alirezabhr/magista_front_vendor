21e910136d476120cc91675291ffc452f58d9289
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-22 23:05:39 +0330

    userPassword removed and checkUserExistence action changed. it returns the response in then

--- store/auth/index.js
+++ store/auth/index.js
@@ -1,18 +1,14 @@
 import axios from 'axios'
 
-const state = {
+const state = () => ({
   userPhone: '',
-  userPassword: '',
   userToken: localStorage.getItem('MagistaToken') || '',
   userId: localStorage.getItem('MagistaId') || 0
-}
+})
 
 const mutations = {
   setUserPhone (state, phone) {
     state.userPhone = phone
-  },
-  setUserPassword (state, password) {
-    state.userPassword = password
   },
   setUserToken (state, token) {
     if (token == null) {
@@ -31,39 +27,48 @@ const mutations = {
 }
 
 const actions = {
-  checkUserExistence (context, payload) {
-    return axios.get(
-      'http://magista.ir/user/',
-      payload
-    ).then(() => {
+  checkUserExistence (vuexContext, payload) {
+    const url = 'http://127.0.0.1:8000/' + 'user/'
 
+    return axios.post(
+      url,
+      payload
+    ).then((response) => {
+      vuexContext.commit('setUserPhone', payload.phone)
+      return response
     }).catch((e) => {
-      print(e.response)
+      throw e.response
     })
   },
-  sendOtp ({ context, rootGetters }, payload) {
-    const url = rootGetters['general/getBaseUrl'] + 'send-otp/'
+  requestOtpCode (vuexContext, payload) {
+    const url = 'http://127.0.0.1:8000/' + 'send-otp/'
 
     return axios.post(
       url,
       payload
     ).then(() => {
-      context.commit('setUserPhone', payload.phone)
+      vuexContext.commit('setUserPhone', payload.phone)
     }).catch((e) => {
       throw e.response
     })
   },
-  checkOtp (context, payload) {
+  checkOtpCode (vuexContext, payload) {
+    const url = 'http://127.0.0.1:8000/' + 'check-otp/'
+    payload.phone = vuexContext.getters.getUserPhone
+
     return axios.post(
-      'http://magista.ir/check-otp/',
+      url,
       payload
     ).catch((e) => {
       throw e.response
     })
   },
-  signup (context, payload) {
+  userSignup (vuexContext, payload) {
+    const url = 'http://127.0.0.1:8000/' + 'signup/'
+    payload.phone = vuexContext.getters.getUserPhone
+
     return axios.post(
-      'http://magista.ir/signup/',
+      url,
       payload
     ).then((response) => {
       const data = response.data
@@ -71,15 +76,20 @@ const actions = {
       const token = data.token
       const id = data.id
 
-      context.commit('setUserToken', token)
-      context.commit('setUserId', id)
+      vuexContext.commit('setUserToken', token)
+      vuexContext.commit('setUserId', id)
     }).catch((e) => {
       throw e.response
     })
   },
-  login (context, payload) {
+  userLogin (vuexContext, payload) {
+    const url = 'http://127.0.0.1:8000/' + 'login/'
+    payload.phone = vuexContext.getters.getUserPhone
+    console.log('payload in login: ')
+    console.log(payload)
+
     return axios.post(
-      'http://magista.ir/login/',
+      url,
       payload
     ).then((response) => {
       const data = response.data
@@ -87,13 +97,13 @@ const actions = {
       const token = data.token
       const id = response.data.id
 
-      context.commit('setUserToken', token)
-      context.commit('setUserId', id)
+      vuexContext.commit('setUserToken', token)
+      vuexContext.commit('setUserId', id)
     }).catch((e) => {
       throw e.response
     })
   },
-  logout (context) {
+  userLogout (context) {
     context.commit('setUserToken', null)
     context.commit('profile/setUserData', {}, { root: true })
   }
@@ -102,9 +112,6 @@ const actions = {
 const getters = {
   getUserPhone: (state) => {
     return state.userPhone
-  },
-  getUserPassword: (state) => {
-    return state.userPassword
   },
   getUserId: (state) => {
     return state.userId
