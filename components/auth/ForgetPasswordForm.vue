109a8ed2b4a360c53002fc3bb255f13d79335c28
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-23 13:55:53 +0330

    forget password component created

---
+++ components/auth/ForgetPasswordForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card class="pb-4">
+    <v-form
+      ref="form"
+      lazy-validation
+      @submit.prevent="validateAndSubmitForm"
+    >
+      <v-card-title>
+        <v-col>
+          <div class="text-h6 font-weight-bold">تغییر رمز عبور</div>
+          <div class="text-subtitle-2 font-weight-light pt-2">رمز عبور خود را انتخاب کنید.</div>
+        </v-col>
+      </v-card-title>
+
+      <v-col class="py-0 px-8">
+        <v-text-field
+          v-model="password"
+          :prepend-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="passwordRules"
+          :type="showPassword ? 'text' : 'password'"
+          label="رمز عبور جدید"
+          hint="حداقل 8 کاراکتر"
+          outlined
+          dense
+          class="py-2"
+          style="direction: ltr"
+          @click:prepend="showPassword = !showPassword"
+        />
+      </v-col>
+
+      <v-card-actions class="px-8">
+        <v-btn
+          block
+          rounded
+          class="primary font-weight-bold py-5"
+          type="submit"
+        >
+          ثبت رمز عبور و ورود
+        </v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ForgetPasswordForm',
+  data () {
+    return {
+      password: '',
+      showPassword: false,
+      passwordRules: [
+        value => !!value || 'رمز عبور الزامی می‌باشد.',
+        v => v.length >= 8 || 'رمز عبور باید حداقل 8 کاراکتر باشد.'
+      ]
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submitForm', this.password)
+      }
+    }
+  }
+}
+</script>
