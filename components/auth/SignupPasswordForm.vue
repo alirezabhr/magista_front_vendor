73c78b3f5ceb5e80405eba104fb5b24e9481575a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-23 09:39:06 +0330

    rules added for password field

--- components/auth/SignupPasswordForm.vue
+++ components/auth/SignupPasswordForm.vue
@@ -1,35 +1,43 @@
 <template>
   <v-card class="pb-4">
-    <v-card-title>
-      <v-col>
-        <div class="text-h6 font-weight-bold">ثبت نام در مگیستا</div>
-        <div class="text-subtitle-2 font-weight-light pt-2">رمز عبور خود را انتخاب کنید.</div>
+    <v-form
+      ref="form"
+      lazy-validation
+    >
+      <v-card-title>
+        <v-col>
+          <div class="text-h6 font-weight-bold">ثبت نام در مگیستا</div>
+          <div class="text-subtitle-2 font-weight-light pt-2">رمز عبور خود را انتخاب کنید.</div>
+        </v-col>
+      </v-card-title>
+
+      <v-col class="py-0 px-8">
+        <v-text-field
+          v-model="password"
+          :prepend-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="rules"
+          :type="showPassword ? 'text' : 'password'"
+          label="رمز عبور جدید"
+          hint="حداقل 8 کاراکتر"
+          outlined
+          dense
+          class="py-2"
+          style="direction: ltr"
+          @click:prepend="showPassword = !showPassword"
+        />
       </v-col>
-    </v-card-title>
 
-    <v-col class="py-0 px-8">
-      <div class="text-subtitle-2 grey--text">
-        حداقل 8 کاراکتر
-      </div>
-      <v-text-field
-        v-model="password"
-        label="رمز عبور جدید"
-        outlined
-        dense
-        class="py-2"
-      />
-    </v-col>
-
-    <v-card-actions class="px-8">
-      <v-btn
-        block
-        rounded
-        class="primary font-weight-bold py-5"
-        @click.prevent="$emit('submitForm', password)"
-      >
-        ثبت رمز عبور و ورود
-      </v-btn>
-    </v-card-actions>
+      <v-card-actions class="px-8">
+        <v-btn
+          block
+          rounded
+          class="primary font-weight-bold py-5"
+          @click.prevent="validateAndSubmitForm"
+        >
+          ثبت رمز عبور و ورود
+        </v-btn>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
@@ -38,7 +46,19 @@ export default {
   name: 'SignupPasswordForm',
   data () {
     return {
-      password: ''
+      password: '',
+      showPassword: false,
+      rules: [
+        value => !!value || 'رمز عبور الزامی می‌باشد.',
+        v => v.length >= 8 || 'رمز عبور باید حداقل 8 کاراکتر باشد.'
+      ]
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submitForm', this.password)
+      }
     }
   }
 }
