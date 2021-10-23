288d0b92e622c2e99a0acf6395526a6ce6bea52d
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-23 09:31:03 +0330

    rules added for password text field

--- components/auth/LoginPasswordForm.vue
+++ components/auth/LoginPasswordForm.vue
@@ -1,61 +1,69 @@
 <template>
   <v-card class="pb-4">
-    <v-card-title>
-      <v-col>
-        <div class="text-h6 font-weight-bold">ورود در مگیستا</div>
-        <div class="text-subtitle-2 grey--text text--darken-1 pt-2">شما قبلا در مگیستا ثبت‌نام کرده‌اید. لطفا رمز عبور خود را وارد کنید.</div>
-      </v-col>
-    </v-card-title>
+    <v-form
+      ref="form"
+      lazy-validation
+    >
+      <v-card-title>
+        <v-col>
+          <div class="text-h6 font-weight-bold">ورود در مگیستا</div>
+          <div class="text-subtitle-2 grey--text text--darken-1 pt-2">شما قبلا در مگیستا ثبت‌نام کرده‌اید. لطفا رمز عبور خود را وارد کنید.</div>
+        </v-col>
+      </v-card-title>
 
-    <v-col class="py-0 px-8">
-      <div class="text-subtitle-2 grey--text">
-        حداقل 8 کاراکتر
-      </div>
-      <v-text-field
-        v-model="password"
-        label="رمز عبور"
-        outlined
-        dense
-        class="py-2"
-      />
-    </v-col>
-
-    <v-card-actions>
-      <v-col>
-        <v-row class="px-8">
-          <v-btn
-            block
-            rounded
-            class="primary font-weight-bold py-5"
-            @click.prevent="$emit('submitForm', password)"
-          >
-            ورود
-          </v-btn>
-        </v-row>
-        <v-row justify="center">
-          <v-btn
-            text
-            color="primary"
-            class="text-subtitle-2 my-4 px-2"
-            @click.prevent="$emit('changePhone')"
-          >
-            <v-icon left>
-              mdi-pencil
-            </v-icon>
-            تغییر شماره موبایل
-          </v-btn>
-          <v-spacer />
-          <v-btn
-            text
-            color="primary"
-            class="text-subtitle-2 my-4 px-2"
-            @click.prevent="$emit('forgetPassword')"
-          >
-            رمز عبورم را فراموش کرده‌ام
-          </v-btn>
-        </v-row>
+      <v-col class="py-0 px-8">
+        <v-text-field
+          v-model="password"
+          :prepend-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="rules"
+          :type="showPassword ? 'text' : 'password'"
+          label="رمز عبور"
+          hint="حداقل 8 کاراکتر"
+          outlined
+          dense
+          class="py-2"
+          style="direction: ltr"
+          @click:prepend="showPassword = !showPassword"
+        />
       </v-col>
-    </v-card-actions>
+
+      <v-card-actions>
+        <v-col>
+          <v-row class="px-8">
+            <v-btn
+              block
+              rounded
+              class="primary font-weight-bold py-5"
+              @click.prevent="validateAndSubmitForm"
+            >
+              ورود
+            </v-btn>
+          </v-row>
+          <v-row justify="center">
+            <v-btn
+              text
+              color="primary"
+              class="text-subtitle-2 my-4 px-2"
+              @click.prevent="$emit('changePhone')"
+            >
+              <v-icon left>
+                mdi-pencil
+              </v-icon>
+              تغییر شماره موبایل
+            </v-btn>
+            <v-spacer />
+            <v-btn
+              text
+              color="primary"
+              class="text-subtitle-2 my-4 px-2"
+              @click.prevent="$emit('forgetPassword')"
+            >
+              رمز عبورم را فراموش کرده‌ام
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
@@ -64,7 +72,19 @@ export default {
   name: 'LoginPasswordForm',
   data () {
     return {
-      password: ''
+      password: '',
+      showPassword: false,
+      rules: [
+        value => !!value || '.رمز عبور الزامی می‌باشد',
+        v => v.length >= 8 || '.رمز عبور باید حداقل 8 کاراکتر باشد'
+      ]
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submitForm', this.password)
+      }
     }
   }
 }
