36bd710cf1e88481c8336ee5e3d77349500fc3cf
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-23 15:17:45 +0330

    middleware added to check if user is authenticated

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,6 @@
 export default function ({ store, redirect }) {
-  // eslint-disable-next-line no-console
-  console.log('[ Middleware ] auth is running')
-
   // If the user is not authenticated
-  // if (!store.state.authenticated) {
-  //   return redirect('/login')
-  // }
+  if (!store.getters['auth/isAuthenticated']) {
+    return redirect('/auth')
+  }
 }
