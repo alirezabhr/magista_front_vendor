cad142096b3a8950beff7f1d299eb96ef20c6224
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-24 13:00:13 +0330

    instagram id form created for first step of creating shop

---
+++ components/create_shop/InstagramIdForm.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-card class="pb-4">
+    <v-form
+      ref="form"
+      lazy-validation
+      @submit.prevent="validateAndSubmitForm"
+    >
+      <v-card-title>
+        <v-col>
+          <div class="text-h6 font-weight-bold">ساخت فروشگاه</div>
+          <div class="text-subtitle-2 font-weight-light pt-2">آیدی اینستاگرام خود را وارد کنید.</div>
+        </v-col>
+      </v-card-title>
+
+      <v-col class="py-0 px-8">
+        <div class="text-subtitle-2 grey--text">
+          مثال: alireza_bhr
+        </div>
+        <v-text-field
+          v-model="instagramId"
+          label="آیدی اینستاگرام"
+          :rules="igIdRules"
+          prefix="@"
+          outlined
+          dense
+          class="py-2"
+          style="direction: ltr"
+        />
+      </v-col>
+
+      <v-card-actions class="px-8">
+        <v-col>
+          <v-btn
+            block
+            class="primary font-weight-bold py-5"
+            :loading="isSubmittingIgId"
+            type="submit"
+          >
+            تایید
+          </v-btn>
+          <v-row justify="center">
+            <v-btn
+              text
+              :disabled="isSubmittingIgId"
+              color="primary"
+              class="text-subtitle-2 my-4 px-2"
+              @click.prevent="$router.push('/dashboard')"
+            >
+              بازگشت به پیشخوان
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'InstagramIdForm',
+  props: {
+    isSubmittingIgId: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+      instagramId: '',
+      igIdRules: [
+        value => !!value || '.آیدی اینستاگرام الزامی می‌باشد',
+        value => !value.includes(' ') || '.آیدی اینستاگرام نامعتبر است',
+        value => !value.includes('-') || '.آیدی اینستاگرام نامعتبر است'
+      ]
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submitForm', this.instagramId)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.ltr {
+    direction: ltr;
+    text-align: left;
+    display: inline-block;
+}
+</style>
