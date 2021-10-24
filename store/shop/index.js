706ec6dc74f94e49e607961f030be9807a170170
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-24 13:02:27 +0330

    get vendor shops action added

--- store/shop/index.js
+++ store/shop/index.js
@@ -11,7 +11,19 @@ const mutations = {
 }
 
 const actions = {
+  getVendorShops (vuexContext) {
+    const userPk = vuexContext.rootGetters['auth/getUserId']
+    axios.defaults.headers.common.Authorization = vuexContext.rootGetters['auth/getUserToken']
 
+    const url = 'http://127.0.0.1:8000/' + 'shop/' + `${userPk}`
+
+    return axios.get(url).then((response) => {
+      vuexContext.commit('setShopsName', response.data)
+      return response.data
+    }).catch((e) => {
+      throw e.response
+    })
+  }
 }
 
 const getters = {
