b8de0c563738ba65fba3d5259eb46d0b988ad4d9
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-26 14:15:06 +0330

    actions added for getting and saving media query files and removing extra posts. also for creating an online shop

--- store/shop/index.js
+++ store/shop/index.js
@@ -1,12 +1,20 @@
 import axios from 'axios'
 
 const state = () => ({
-  shopsName: []
+  shopsName: [],
+  instagramUsername: null,
+  userProfileInfo: null
 })
 
 const mutations = {
   setShopsName (state, shops) {
     state.shopsName = shops
+  },
+  setInstagramUsername (state, username) {
+    state.instagramUsername = username
+  },
+  setUserProfileInfo (state, profileInfo) {
+    state.userProfileInfo = profileInfo
   }
 }
 
@@ -24,12 +32,66 @@ const actions = {
       throw e.response
     })
   },
-  getInstagramMedia (vuexContext, payload) {
+  saveInstagramMediaQueryFile (vuexContext, instagramUsername) {
     const url = 'http://127.0.0.1:8000/' + 'user-media/'
     axios.defaults.headers.common.Authorization = vuexContext.rootGetters['auth/getUserToken']
 
-    return axios.get(url, { params: payload }).then((response) => {
+    const queryParams = {
+      instagram_username: instagramUsername
+    }
+
+    return axios.post(url, { params: queryParams }).then((response) => {
+      vuexContext.commit('setInstagramUsername', instagramUsername)
+      vuexContext.commit('setUserProfileInfo', response.data)
+    }).catch((e) => {
+      throw e.response
+    })
+  },
+  getInstagramMediaQueryFile (vuexContext) {
+    const url = 'http://127.0.0.1:8000/' + 'user-media/'
+    axios.defaults.headers.common.Authorization = vuexContext.rootGetters['auth/getUserToken']
+
+    const queryParams = {
+      instagram_username: vuexContext.getters.getInstagramUsername
+    }
+
+    return axios.get(url, { params: queryParams }).then((response) => {
       return response.data
+    }).catch((e) => {
+      throw e.response
+    })
+  },
+  removeExtraMediaQuery (vuexContext, payload) {
+    const userPk = vuexContext.rootGetters['auth/getUserId']
+    const url = 'http://127.0.0.1:8000/' + 'user-media/'
+    axios.defaults.headers.common.Authorization = vuexContext.rootGetters['auth/getUserToken']
+
+    const queryParams = {
+      instagram_username: vuexContext.getters.getInstagramUsername
+    }
+
+    payload.user_id = userPk
+
+    return axios.put(url, payload, { params: queryParams }).catch((e) => {
+      throw e.response
+    })
+  },
+  createShop (vuexContext, payload) {
+    const userPk = vuexContext.rootGetters['auth/getUserId']
+    axios.defaults.headers.common.Authorization = vuexContext.rootGetters['auth/getUserToken']
+
+    const url = 'http://127.0.0.1:8000/' + 'shop/' + `${userPk}`
+
+    console.log('in createShop action')
+    console.log(vuexContext.getters.getInstagramUsername)
+
+    payload.vendor = userPk
+    payload.instagram_username = vuexContext.getters.getInstagramUsername
+    payload.instagram_id = vuexContext.getters.getUserProfileInfo.instagram_id
+
+    return axios.post(url, payload).then((response) => {
+      console.log('then in createShop')
+      console.log(response.data)
     }).catch((e) => {
       throw e.response
     })
@@ -37,7 +99,12 @@ const actions = {
 }
 
 const getters = {
-
+  getInstagramUsername: (state) => {
+    return state.instagramUsername
+  },
+  getUserProfileInfo: (state) => {
+    return state.userProfileInfo
+  }
 }
 
 export default {
