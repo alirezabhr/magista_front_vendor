3dd9af0352d962c9a479176f142944e040c236d4
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-10-26 18:59:36 +0330

    rules added for all text fields

--- components/create_shop/ShopDataForm.vue
+++ components/create_shop/ShopDataForm.vue
@@ -13,40 +13,58 @@
 
       <v-col class="py-0 px-8">
         <v-text-field
-          v-model="email"
+          v-model="shopData.email"
           label="ایمیل"
           :rules="emailRules"
           outlined
           dense
-          class="py-2"
           style="direction: ltr"
         />
-        <v-text-field
-          v-model="instagramId"
-          label="آیدی اینستاگرام"
-          :rules="igIdRules"
+        <v-row>
+          <v-col cols="12" md="6" class="my-0 py-0">
+            <v-autocomplete
+              v-model="shopData.province"
+              label="استان"
+              :items="getAllProvinces"
+              :rules="provinceRules"
+              outlined
+              dense
+            >
+              <template #no-data>
+                <v-list-item>
+                  <v-list-item-title>
+                    هیچ استانی با این مشخصات پیدا نشد.
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-autocomplete>
+          </v-col>
+
+          <v-col cols="12" md="6" class="my-0 py-0">
+            <v-autocomplete
+              v-model="shopData.city"
+              label="شهر"
+              :items="getProvinceCities"
+              :rules="cityRules"
+              outlined
+              dense
+            >
+              <template #no-data>
+                <v-list-item>
+                  <v-list-item-title>
+                    هیچ شهری با این مشخصات پیدا نشد.
+                  </v-list-item-title>
+                </v-list-item>
+              </template>
+            </v-autocomplete>
+          </v-col>
+        </v-row>
+        <v-textarea
+          v-model="shopData.address"
+          label="آدرس"
+          :rules="addressRules"
           outlined
           dense
-          class="py-2"
-          style="direction: ltr"
-        />
-        <v-text-field
-          v-model="instagramId"
-          label="آیدی اینستاگرام"
-          :rules="igIdRules"
-          outlined
-          dense
-          class="py-2"
-          style="direction: ltr"
-        />
-        <v-text-field
-          v-model="instagramId"
-          label="آیدی اینستاگرام"
-          :rules="igIdRules"
-          outlined
-          dense
-          class="py-2"
-          style="direction: ltr"
         />
       </v-col>
 
@@ -55,7 +73,7 @@
           <v-btn
             block
             class="primary font-weight-bold py-5"
-            :loading="isSubmittingIgId"
+            :loading="isSubmittingForm"
             type="submit"
           >
             ثبت و ادامه
@@ -67,12 +85,59 @@
 </template>
 
 <script>
+import provincesAndCities from '@/assets/json/iran_cities.json'
+
 export default {
   name: 'ShopDataForm',
   props: {
     isSubmittingForm: {
       type: Boolean,
       required: true
+    }
+  },
+  data () {
+    return {
+      shopData: {
+        email: '',
+        province: '',
+        city: '',
+        address: ''
+      },
+      emailRules: [
+        value => !!value || '.ایمیل الزامی می‌باشد',
+        value => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || '.ایمیل نامعتبر است'
+      ],
+      provinceRules: [
+        value => !!value || 'استان الزامی است.',
+        value => this.getAllProvinces.includes(value) || 'استان نامعتبر است.'
+      ],
+      cityRules: [
+        value => !!value || 'شهر الزامی است.',
+        value => this.getProvinceCities.includes(value) || 'شهر نامعتبر است.'
+      ],
+      addressRules: [
+        value => !!value || 'آدرس الزامی می‌باشد.',
+        value => !/^\s+$/.test(value) || 'آدرس نامعتبر است.'
+      ]
+    }
+  },
+  computed: {
+    getAllProvinces () {
+      return Array.from(provincesAndCities, item => item.name)
+    },
+    getProvinceCities () {
+      if (this.getAllProvinces.includes(this.shopData.province)) {
+        const province = provincesAndCities.find(province => province.name === this.shopData.province)
+        return Array.from(province.cities, item => item.name)
+      }
+      return []
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submit', this.shopData)
+      }
     }
   }
 }
