316996f75aa6220acc74cb6b7ac0d30b15d01073
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-18 18:44:54 +0330

    PostPreview model added

---
+++ models/post_preview.ts
@@ -0,0 +1,13 @@
+class PostPreview {
+    index: number
+    id: number
+    thumbnailSrc: string
+
+    constructor(index: number, id: number, thumbnailSrc: string) {
+        this.index = index
+        this.id = id
+        this.thumbnailSrc = thumbnailSrc
+    }
+}
+
+export default PostPreview
