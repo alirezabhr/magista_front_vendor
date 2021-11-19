fc2fb992c1ecbba554d18d3333e4698dbc24d0dc
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-19 20:13:39 +0330

    ProductDiscountForm component created

---
+++ components/product/ProductDiscountForm.vue
@@ -0,0 +1,85 @@
+<template>
+    <v-card>
+        <v-form
+            ref="form"
+            lazy-validation
+            @submit.prevent="validateAndSubmitForm"
+        >
+        <v-card-title>
+            <v-col>
+                <div class="text-h6 font-weight-bold">اعمال تخفیف</div>
+            </v-col>
+        </v-card-title>
+
+        <v-col class="py-0 px-8">
+            <v-text-field
+                v-model="discount"
+                label="درصد تخفیف"
+                :rules="discountRules"
+                type="number"
+                prefix="%"
+                outlined
+                dense
+            />
+        </v-col>
+
+        <v-card-actions class="px-8">
+            <v-col cols="12">
+                <v-row justify="center">
+                    <v-btn
+                        block
+                        class="primary font-weight-bold py-5"
+                        :loading="isSubmittingForm"
+                        type="submit"
+                    >
+                        ثبت تخفیف
+                    </v-btn>
+                </v-row>
+                <v-row justify="center">
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="$emit('close')"
+                    >
+                        بستن
+                    </v-btn>
+                </v-row>
+            </v-col>
+        </v-card-actions>
+        </v-form>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ProductDiscountForm',
+  props: {
+    isSubmittingForm: {
+      type: Boolean,
+      required: true
+    },
+    productDiscount: {
+      type: Number,
+      required: false
+    }
+  },
+  data () {
+    return {
+      discount: '',
+      discountRules: [
+        value => !!value || 'درصد تخفیف را وارد کنید.',
+        value => value >= 0 || 'تخفیف نامعتبر است.',
+        value => value <= 99 || 'تخفیف نامعتبر است.',
+        value => !value.includes('.') || 'تخفیف نامعتبر است.'
+      ],
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submit', Number.parseInt(this.discount))
+      }
+    }
+  }
+}
+</script>
