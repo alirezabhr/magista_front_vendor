064c5d7b5897514a2a3e364f070cdc4a4fabbfa4
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-19 11:47:03 +0330

    current shop and ProductPreview component added

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,66 +1,81 @@
 <template>
-  <v-col v-if="!hasShops" cols="12" class="">
-    <v-row class="pa-10 text-h5 font-weight-bold grey--text" justify="center">
-      هیچ فروشگاهی ندارید!
-    </v-row>
-    <v-row justify="center">
-      <v-img
-        contain
-        max-height="250"
-        max-width="250"
-        src="https://picsum.photos/id/11/500/300"
-      />
-    </v-row>
-    <v-row class="pa-10 text-h5 font-weight-bold grey--text" justify="center">
-      <v-btn class="primary font-weight-bold py-5" @click.prevent="$router.push('/create-shop')">
-        ساخت فروشگاه جدید
-      </v-btn>
-    </v-row>
-  </v-col>
-  <v-col v-else cols="12" class="red lighten-4">
-    <v-row>
-      hello
-    </v-row>
-    <v-row>
-      bye bye
-    </v-row>
-  </v-col>
+  <div v-if="isLoadingPage">
+    <v-col cols="12">
+      loading ...
+    </v-col>
+  </div>
+  <div v-else>
+    <v-col v-if="getShops.length === 0" cols="12">
+      <v-row class="pa-10 text-h5 font-weight-bold grey--text" justify="center">
+        هیچ فروشگاهی ندارید!
+      </v-row>
+      <v-row justify="center">
+        <v-img
+          contain
+          max-height="250"
+          max-width="250"
+          :src="getEmptyStateImageUrl"
+        />
+      </v-row>
+      <v-row class="pa-10 text-h5 font-weight-bold grey--text" justify="center">
+        <v-btn
+          class="primary font-weight-bold py-5"
+          @click.prevent="$router.push('/create-shop')"
+        >
+          ساخت فروشگاه جدید
+        </v-btn>
+      </v-row>
+    </v-col>
+    <v-col v-else cols="12" class="ma-0 pa-0">
+      <v-row no-gutters class="ma-0 pa-0" dir="ltr">
+        <v-col cols="4" v-for="prod in getCurrentShopProducts" :key="prod.id" class="ma-0 pa-0">
+          <ProductPreview :product="prod" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+import ProductPreview from '@/components/ProductPreview.vue'
 
 export default {
   name: 'DashboardPage',
+  components: {
+    ProductPreview
+  },
   layout: 'panel',
   data () {
     return {
-      hasShops: false
+      isLoadingPage: false
     }
   },
-  mounted () {
-    this.getUserShops()
+  async mounted () {
+    this.isLoadingPage = true
+    await this.getUserShops()
+    if (this.getShops.length > 0) {
+      await this.currentShopProducts()
+    }
+    this.isLoadingPage = false
   },
   methods: {
-    ...mapActions('shop', ['getVendorShops', 'createShop']),
+    ...mapActions('shop', ['getVendorShops', 'createShop', 'currentShopProducts']),
 
-    getUserShops () {
-      this.getVendorShops().then((shopsList) => {
-        if (shopsList.length === 0) {
-          console.log('empty list')
-          this.hasShops = false
-        } else {
-          console.log('you have shops')
-          this.hasShops = true
-          console.log(shopsList)
-        }
-      }).catch((response) => {
-        console.log('catch in getUserShops')
+    async getUserShops () {
+      await this.getVendorShops().catch((response) => {
         for (const key in response.data) {
           this.snackbarMessage = response.data[key][0]
           this.showSnackbar = true
         }
       })
+    }
+  },
+  computed: {
+    ...mapGetters('shop', ['getShops', 'getCurrentShopProducts']),
+
+    getEmptyStateImageUrl() {
+      return process.env.baseURL + 'media/source/empty_state.png'
     }
   }
 }
