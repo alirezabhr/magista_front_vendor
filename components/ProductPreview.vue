3c398c7f25a149fe9d99ec06001209024047d597
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-19 11:47:43 +0330

    ProductionPreview component created

---
+++ components/ProductPreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card flat class="pa-0 ma-0 blue" rounded="0">
+    <v-img :aspect-ratio="1" :src="this.productImageUrl" class="pa-0 ma-0" />
+  </v-card>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import Product from '~/models/product'
+
+export default {
+  name: 'ProductPreview',
+  props: {
+    product: {
+      type: Object as PropType<Product>,
+      required: true
+    }
+  },
+  computed: {
+    productImageUrl () {
+      return process.env.baseURL + this.product.displayImage
+    }
+  }
+}
+</script>
