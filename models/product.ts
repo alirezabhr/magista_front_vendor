b588ea79ac548c6de81c6b0dd0e5f011518a31ae
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-20 17:47:30 +0330

    discount discription added

--- models/product.ts
+++ models/product.ts
@@ -12,13 +12,14 @@ class Product {
   isExisting: boolean
   discountPercent: number | null
   discountAmount: number | null
+  discountDescription: string
   createdAt: Date
   updatedAt: Date
 
   constructor(id: number, shop: number, originalPrice: number, shortcode: string,
     title: string, description: string, displayImage: string, rate: number | null,
     isExisting: boolean, createdAt: Date, updatedAt: Date, discountPercent: number | null,
-    discountAmount: number | null) {
+    discountAmount: number | null, discountDescription: string) {
 
     this.id = id
     this.shop = shop
@@ -31,6 +32,7 @@ class Product {
     this.isExisting = isExisting
     this.discountPercent = discountPercent
     this.discountAmount = discountAmount
+    this.discountDescription = discountDescription
     this.createdAt = createdAt
     this.updatedAt = updatedAt
   }
