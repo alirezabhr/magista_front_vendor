1996fe3804bc59c4b31cf93742c1f863ca7f04fb
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-20 15:51:29 +0330

    discount amount and final price added.

--- components/product/ProductDiscountForm.vue
+++ components/product/ProductDiscountForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card>
+    <v-card v-if="productPrice !== null">
         <v-form
             ref="form"
             lazy-validation
@@ -14,18 +14,29 @@
         </v-card-title>
 
         <v-col class="py-0 px-8">
+            <v-row v-show="finalPrice" no-gutters class="pb-2 text-body1" align="center">
+              <div class="text-decoration-line-through font-weight-medium">{{ productPrice }}</div>
+              <v-icon class="px-2">mdi-chevron-triple-left</v-icon>
+              <div class="font-weight-bold primary--text">{{ finalPrice }}</div>
+            </v-row>
             <v-text-field
-                v-model="discount"
+                v-model="discountPercent"
                 label="درصد تخفیف"
-                :rules="discountRules"
+                :rules="discountPercentRules"
                 type="number"
                 prefix="%"
                 outlined
                 dense
             />
+            <v-textarea
+              v-model="discountDescription"
+              label="توضیح (اختیاری)"
+              :rules="discountDescriptionRules"
+              outlined
+              dense
+            />
         </v-col>
-
-        <v-card-actions class="px-8 pb-5">
+        <v-card-actions class="px-8 pb-5 pt-4">
             <v-col cols="12">
                 <v-row justify="center">
                     <v-btn
@@ -41,6 +52,20 @@
         </v-card-actions>
         </v-form>
     </v-card>
+    <v-card v-else class="py-2">
+      <v-card-title>
+        <v-row>
+          <v-icon color="red">mdi-alert-decagram-outline</v-icon>
+          <div class="text-body2 pr-2 py-2">ابتدا قیمت محصول را وارد نمایید.</div>
+        </v-row>
+      </v-card-title>
+      <v-card-text class="text-subtitle1 py-3">
+        <div class="px-5">برای وارد کردن قیمت میتوانید از گزینه تغییر قیمت اقدام کنید.</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text class="font-weight-light blue--text mx-auto" @click.prevent="$emit('close')">بستن</v-btn>
+      </v-card-actions>
+    </v-card>
 </template>
 
 <script>
@@ -51,6 +76,10 @@ export default {
       type: Boolean,
       required: true
     },
+    productPrice: {
+      type: Number | null,
+      required: true
+    },
     productDiscount: {
       type: Number,
       required: false
@@ -58,25 +87,43 @@ export default {
   },
   mounted() {
     if (this.productDiscount) {
-      this.discount = this.productDiscount
+      this.discountPercent = this.productDiscount
     }
   },
   data () {
     return {
-      discount: '',
-      discountRules: [
+      discountPercent: '',
+      discountDescription: '',
+      discountPercentRules: [
         value => !!value || 'درصد تخفیف را وارد کنید.',
         value => value >= 0 || 'تخفیف نامعتبر است.',
         value => value <= 99 || 'تخفیف نامعتبر است.',
         value => !value.includes('.') || 'تخفیف نامعتبر است.'
       ],
+      discountDescriptionRules: [
+        value => value.length <= 300 || 'توضیحات بسیار طولانی است.'
+      ]
     }
   },
   methods: {
     validateAndSubmitForm () {
       if (this.$refs.form.validate()) {
-        this.$emit('submit', Number.parseInt(this.discount))
+        this.$emit('submit', Number.parseInt(this.discountPercent), this.dicountAmount, this.discountDescription)
       }
+    }
+  },
+  computed: {
+    dicountAmount() {
+      let fp = this.productPrice * Number.parseInt(this.discountPercent) / 100
+      fp = Number.parseInt(fp / 1000)
+      return fp*1000
+    },
+    finalPrice () {
+      let final = this.productPrice - this.dicountAmount
+      if (final < 0) {
+        final = 0
+      }
+      return final
     }
   }
 }
