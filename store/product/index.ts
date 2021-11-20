1cd61fbeaa34962749f0e787e96ecf7ee0856661
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-20 17:47:09 +0330

    editProduct and create discount actions implemented

--- store/product/index.ts
+++ store/product/index.ts
@@ -17,7 +17,7 @@ const mutations = <MutationTree<ProductState>>{
     state.product = new Product(prod.id, prod.shop, prod.originalPrice, prod.shortcode,
                                 prod.title, prod.description, prod.displayImage, prod.rate,
                                 prod.isExisting, prod.createdAt, prod.updatedAt,
-                                prod.discountPercent, prod.discountAmount)
+                                prod.discountPercent, prod.discountAmount, prod.discountDescription)
   }
 }
 
@@ -27,6 +27,26 @@ const actions = <ActionTree<ProductState, RootState>>{
 
     return this.$client.get(url).then((response) => {
       vuexContext.commit('setProduct', response.data)
+    }).catch((e) => {
+      throw e.response
+    })
+  },
+  editProduct(vuexContext, product) {
+    const url = process.env.baseURL + `shop/product/${product.shortcode}/`
+
+    return this.$client.put(url, product).then((response) => {
+      vuexContext.commit('setProduct', response.data)
+    }).catch((e) => {
+      throw e.response
+    })
+  },
+  createProductDiscount(vuexContext, payloadData) {
+    const product = vuexContext.getters.getProduct
+    const url = process.env.baseURL + `shop/product/${product.shortcode}/discount/`
+    payloadData.product = product.id
+
+    return this.$client.post(url, payloadData).then((response) => {
+      vuexContext.commit('setProduct', response.data.product)
     }).catch((e) => {
       throw e.response
     })
