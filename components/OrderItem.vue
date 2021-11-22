b20661b9c69aadc60abc79aec8b583eff0e6ab88
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-22 20:09:37 +0330

    OrderItem component created

---
+++ components/OrderItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card flat rounded="lg">
+    {{ order.count }}
+  </v-card>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import OrderItem from '~/models/order_item'
+
+export default {
+  name: 'OrderItem',
+  props: {
+    order: {
+      type: Object as PropType<OrderItem>,
+      required: true
+    }
+  }
+}
+</script>
