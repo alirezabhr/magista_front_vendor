dc82ec5976a02e87beb524ae50e03a78a06d8a97
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 13:50:37 +0330

    shop and customer fattributes changed

--- models/invoice.ts
+++ models/invoice.ts
@@ -1,15 +1,17 @@
 import OrderItem from './order_item'
+import Shop from './shop'
+import Customer from './customer'
 
 class Invoice {
     id: number
     status: number
-    shop: number
-    customer: number
+    shop: Shop
+    customer: Customer
     orders: OrderItem[]
     updatedAt: Date
     createdAt: Date
 
-    constructor (id: number, status: number, shop: number, customer: number, orders: OrderItem[], updatedAt: Date, createdAt: Date) {
+    constructor (id: number, status: number, shop: Shop, customer: Customer, orders: OrderItem[], updatedAt: Date, createdAt: Date) {
       this.id = id
       this.status = status
       this.shop = shop
