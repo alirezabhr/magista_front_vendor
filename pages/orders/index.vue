d07524acb2904fb62123325e2b4429c38a073f8f
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 20:01:37 +0330

    status and reciever added for invoice cards

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -21,16 +21,42 @@
         />
       </v-row>
     </v-col>
-    <v-col v-else cols="12" class="ma-0 pa-0">
-      <v-row v-for="invoice in getInvoiceList" :key="invoice.id" no-gutters class="ma-0 pa-0">
-        <v-card>
-          <v-card-subtitle>
-            {{ invoice.createdAt }}
-          </v-card-subtitle>
-          <v-row v-for="orderItem in invoice.orders" :key="orderItem">
-            <OrderItem :order="orderItem" />
-          </v-row>
-        </v-card>
+    <v-col v-else cols="12" class="ma-0 px-0 pt-0 pb-10">
+      <v-row v-for="(invoice, index) in getInvoiceList" :key="index" no-gutters class="ma-0 py-4 px-0" justify="center">
+        <v-col cols="11">
+          <v-card elevation="6" color="grey lighten-3">
+            <v-col>
+              <v-card-title class="pt-0 pb-6">
+                وضعیت:
+                {{ invoice.statusText }}
+              </v-card-title>
+              <v-card-subtitle class="py-0">
+                {{ invoice.createdAt }}
+              </v-card-subtitle>
+              <v-card-subtitle class="py-0">
+                گیرنده:
+                {{ invoice.customer.name }}
+              </v-card-subtitle>
+              <v-card-subtitle class="py-0">
+                <v-icon>mdi-map-marker-outline</v-icon>
+                {{ invoice.customer.province }} -
+                {{ invoice.customer.city }} -
+                {{ invoice.customer.address }}
+              </v-card-subtitle>
+              <v-divider class="mt-2 pb-6" />
+              <div class="py-2">
+                <v-row v-for="(orderItem, i) in invoice.orders" :key="i" class="px-2" justify="center">
+                  <OrderItem :order="orderItem" />
+                </v-row>
+              </div>
+              <v-row class=" py-2 px-3 text-subtitle1" no-gutters>
+                قیمت کل:
+                {{ calculateTotalPrice(invoice) }}
+                تومان
+              </v-row>
+            </v-col>
+          </v-card>
+        </v-col>
       </v-row>
     </v-col>
   </div>
@@ -38,7 +64,7 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
-import OrderItem from '~/models/order_item'
+import OrderItem from '@/components/OrderItem.vue'
 
 export default {
   name: 'OrdersPage',
@@ -64,7 +90,15 @@ export default {
     this.isLoadingPage = false
   },
   methods: {
-    ...mapActions('invoice', ['shopInvoices'])
+    ...mapActions('invoice', ['shopInvoices']),
+
+    calculateTotalPrice (invoice) {
+      let total = 0
+      invoice.orders.forEach((order) => {
+        total += order.price * order.count
+      })
+      return total
+    }
   }
 }
 </script>
