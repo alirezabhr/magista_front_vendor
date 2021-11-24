9a08cdb19defe456189b8bce976bf784b18a966c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 20:02:49 +0330

    formatted for linter

--- components/OrderItem.vue
+++ components/OrderItem.vue
@@ -1,7 +1,39 @@
 <template>
-  <v-card flat rounded="lg">
-    {{ order.count }}
-  </v-card>
+  <v-col cols="11" class="pa-0">
+    <v-card outlined class="mb-2">
+      <v-row class="pa-2">
+        <v-col cols="3">
+          <v-card outlined>
+            <v-img
+              :aspect-ratio="1"
+              :src="getFullImageUrl(order.product.displayImage)"
+            >
+              <template #placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="grey lighten-2" />
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="1" class="" align-self="center">
+          <v-row class="font-weight-bold" justify="center">
+            ×{{ order.count }}
+          </v-row>
+        </v-col>
+        <v-col cols="7">
+          <v-row no-gutters>
+            <div class="text-truncate">
+              {{ order.product.title }}
+            </div>
+          </v-row>
+          <v-row class="font-weight-bold" no-gutters>
+            {{ order.price }} تومان
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-col>
 </template>
 
 <script lang="ts">
@@ -14,6 +46,11 @@ export default {
     order: {
       type: Object as PropType<OrderItem>,
       required: true
+    }
+  },
+  methods: {
+    getFullImageUrl (src: string) {
+      return process.env.baseURL + src
     }
   }
 }
