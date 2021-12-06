bece3e0afcb5fe90b5923516bd6e26714e0ce0dd
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-06 17:08:33 +0330

    statusText attr added in constructor

--- models/order.ts
+++ models/order.ts
@@ -2,8 +2,6 @@ import OrderItem from './order_item'
 import Shop from './shop'
 import Customer from './customer'
 
-const OrderStatus = ['در انتظار پرداخت', 'پرداخت شده', 'ارسال شده', 'دریافت شده', 'لغو شده']
-
 class Order {
     id: number
     status: number
@@ -15,10 +13,10 @@ class Order {
     updatedAt: Date
     createdAt: Date
 
-    constructor (id: number, status: number, shop: Shop, customer: Customer, orderItems: OrderItem[], totalPrice: number, updatedAt: Date, createdAt: Date) {
+    constructor (id: number, status: number, statusText: string, shop: Shop, customer: Customer, orderItems: OrderItem[], totalPrice: number, updatedAt: Date, createdAt: Date) {
       this.id = id
       this.status = status
-      this.statusText = OrderStatus[status - 1]
+      this.statusText = statusText
       this.shop = shop
       this.customer = customer
       this.orderItems = orderItems
