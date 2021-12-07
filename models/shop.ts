7b2e12b0f32cfc9461780bfe65202de82182c4e9
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-07 21:54:59 +0330

    email and address added in Shop attrs

--- models/shop.ts
+++ models/shop.ts
@@ -1,18 +1,24 @@
 class Shop {
     id: number
     instagramUsername: string
+    email: string
+    address: string
     province: string
     city: string
     wallet: number
     profileImageUrl: string
+    withdrawalAmount: number
 
-    constructor (id:number, igUsername: string, province: string, city: string, wallet:number, profileUrl: string) {
+    constructor (id:number, igUsername: string, email: string, address: string, province: string, city: string, wallet:number, profileUrl: string, withdrawalAmount: number) {
       this.id = id
       this.instagramUsername = igUsername
+      this.email = email
+      this.address = address
       this.province = province
       this.city = city
       this.wallet = wallet
       this.profileImageUrl = profileUrl
+      this.withdrawalAmount = withdrawalAmount
     }
 
     toJSON () {
