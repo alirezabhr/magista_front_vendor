47981e3b9271c23863185bb3a82b882554f123e5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-13 12:20:55 +0330

    product attribute model created

--- models/product.ts
+++ models/product.ts
@@ -1,3 +1,5 @@
+import ProductAttribute from './product_attribute'
+
 class Product {
   id: number
   shop: number
@@ -11,13 +13,14 @@ class Product {
   isExisting: boolean
   discountPercent: number | null
   discountDescription: string
+  attributes: ProductAttribute[]
   createdAt: Date
   updatedAt: Date
 
   constructor (id: number, shop: number, finalPrice: number, shortcode: string,
     title: string, description: string, displayImage: string, originalPrice: number,
     rate: number | null, isExisting: boolean, createdAt: Date, updatedAt: Date,
-    discountPercent: number | null, discountDescription: string) {
+    discountPercent: number | null, discountDescription: string, attributes: ProductAttribute[]) {
     this.id = id
     this.shop = shop
     this.finalPrice = finalPrice
@@ -30,6 +33,7 @@ class Product {
     this.isExisting = isExisting
     this.discountPercent = discountPercent
     this.discountDescription = discountDescription
+    this.attributes = attributes
     this.createdAt = createdAt
     this.updatedAt = updatedAt
   }
---
+++ models/product_attribute.ts
@@ -0,0 +1,13 @@
+class ProductAttribute {
+  id: number
+  name: string
+  value: string
+
+  constructor (id: number, name:string, value:string) {
+    this.id = id
+    this.name = name
+    this.value = value
+  }
+}
+
+export default ProductAttribute
