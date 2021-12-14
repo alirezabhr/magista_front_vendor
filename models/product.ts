0a555c4fcb75dffb28999fa52b3d8b64995ff17f
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-14 20:01:16 +0330

    display image and shortcode removed from product model

--- models/product.ts
+++ models/product.ts
@@ -2,13 +2,10 @@ import ProductAttribute from './product_attribute'
 
 class Product {
   id: number
-  shop: number
   originalPrice: number
   finalPrice: number
-  shortcode: string
   title: string
   description: string
-  displayImage: string
   rate: number | null
   isExisting: boolean
   discountPercent: number | null
@@ -17,18 +14,14 @@ class Product {
   createdAt: Date
   updatedAt: Date
 
-  constructor (id: number, shop: number, finalPrice: number, shortcode: string,
-    title: string, description: string, displayImage: string, originalPrice: number,
-    rate: number | null, isExisting: boolean, createdAt: Date, updatedAt: Date,
+  constructor (id: number, finalPrice: number, title: string, description: string,
+    originalPrice: number, rate: number | null, isExisting: boolean, createdAt: Date, updatedAt: Date,
     discountPercent: number | null, discountDescription: string, attributes: ProductAttribute[]) {
     this.id = id
-    this.shop = shop
     this.finalPrice = finalPrice
     this.originalPrice = originalPrice
-    this.shortcode = shortcode
     this.title = title
     this.description = description
-    this.displayImage = displayImage
     this.rate = rate
     this.isExisting = isExisting
     this.discountPercent = discountPercent
