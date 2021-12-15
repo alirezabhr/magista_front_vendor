f895795cfac0d2b1af3c847c128dc8f818554a00
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-15 21:11:38 +0330

    tag location model created

---
+++ models/tag_location.ts
@@ -0,0 +1,13 @@
+class TagLocation {
+  id: number
+  x: number
+  y: number
+
+  constructor (id: number, x: number, y: number) {
+    this.id = id
+    this.x = x
+    this.y = y
+  }
+}
+
+export default TagLocation
