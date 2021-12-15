3c40f15ca81dbff18dea1eb61d37584cedae5bf1
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-15 22:10:11 +0330

    product row added top of post description

--- pages/product/_shortcode/index.vue
+++ pages/product/_shortcode/index.vue
@@ -72,50 +72,86 @@
           </v-list>
         </v-menu>
       </v-row>
-      <v-carousel
-        v-model="carouselIndex"
-        :show-arrows="getPost.productImages.length > 1"
-        :hide-delimiters="getPost.productImages.length <= 1"
-        hide-delimiter-background
-        class="pa-0 ma-0"
-        height="100%"
+      <div
+        v-for="(productImage, i) in getPost.productImages"
+        :key="i"
       >
-        <v-carousel-item
-          v-for="(productImage, i) in getPost.productImages"
-          :key="i"
+        <v-carousel
+          v-model="carouselIndex"
+          :show-arrows="getPost.productImages.length > 1"
+          :hide-delimiters="getPost.productImages.length <= 1"
+          hide-delimiter-background
+          class="pa-0 ma-0"
+          height="100%"
         >
-          <v-img :src="productImageUrl(productImage)" :aspect-ratio="1">  <!-- @click.prevent="showProductTags = !showProductTags" -->
-            <section v-for="product in productImage.products" v-show="showProductTags" :key="product.id" @click="t()">
-              <!-- tag section -->
-              <div
-                class="tag"
-                :style="`left: ${product.tag.x}%; bottom: ${product.tag.y}%;`"
-                style="transform: translate(-50%, 100%); max-width: 130px;"
-              >
-                <div class="arrow-up" style="opacity: 85%;" />
-                <v-card
-                  rounded="md"
-                  color="black"
-                  max-width="130"
-                  style="opacity: 85%;"
-                  class="px-3 py-1"
-                  flat
-                >
-                  <div class="white--text font-weight-bold text-caption text-truncate pb-1">{{ product.title }}</div>
-                  <v-row v-if="product.discountPercent" class="px-1 white--text font-weight-light text-decoration-line-through" style="font-size: .600rem;" no-gutters>
-                    {{ product.originalPrice }}
-                  </v-row>
-                  <v-row class="white--text font-weight-medium text-caption px-1" no-gutters>
-                    {{ product.finalPrice }}
-                    <v-spacer />
-                    <span class="currency">تومان</span>
-                  </v-row>
-                </v-card>
+          <v-carousel-item>
+            <v-img :src="productImageUrl(productImage)" :aspect-ratio="1" @click.prevent="showProductTags = !showProductTags">
+              <div v-for="prod in productImage.products" v-show="showProductTags" :key="prod.id" @click="t()">
+                <ProductTag :product="prod" />
               </div>
-            </section>
-          </v-img>
-        </v-carousel-item>
-      </v-carousel>
+            </v-img>
+          </v-carousel-item>
+        </v-carousel>
+        <v-col class="pa-3" no-gutters>
+          <div v-for="product in productImage.products" :key="product.id" @click="t()">
+            <v-row class="font-weight-bold" no-gutters>
+              {{ product.title }}
+            </v-row>
+            <v-row no-gutters align="center">
+              <v-rating
+                v-model="product.rate"
+                background-color="yellow accent-4"
+                color="yellow accent-4"
+                readonly
+                half-increments
+                dense
+                size="24"
+              />
+              <div class="grey-text text-darken-1 font-weight-light px-2">
+                <div v-if="product.rate">
+                  ({{ product.rate }})
+                </div>
+                <div v-else>
+                  بدون نظر
+                </div>
+              </div>
+            </v-row>
+            <v-row no-gutters class="px-1">
+              قیمت:
+              <v-row v-if="product.originalPrice" no-gutters class="pr-2">
+                <div v-show="product.discountPercent" class="pr-2 text-decoration-line-through">
+                  {{ product.originalPrice }}
+                </div>
+                <v-icon v-show="product.discountPercent" class="pr-1">mdi-chevron-triple-left</v-icon>
+                <div>
+                  {{ product.finalPrice }}
+                </div>
+                تومان
+              </v-row>
+              <div v-else>
+                ندارد
+              </div>
+            </v-row>
+            <v-row no-gutters class="px-1 pb-3">
+              تخفیف:
+              <div v-if="product.discountPercent" class="px-2 font-weight-bold">
+                <v-icon color="grey darken-2">mdi-percent mdi-18px</v-icon>
+                {{ product.discountPercent }}
+              </div>
+              <div v-else>
+                ندارد
+              </div>
+            </v-row>
+            <v-col v-if="product.attributes.length > 0" class="px-0 py-3">
+              <div class="font-weight-bold text-body-2" no-gutters>مشخصات محصول</div>
+              <v-row v-for="(attr, index) in product.attributes" :key="index" no-gutters>
+                {{ attr.name }}:  {{ attr.value }}
+              </v-row>
+            </v-col>
+            <v-divider />
+          </div>
+        </v-col>
+      </div>
       <v-col class="pa-3" no-gutters>
         <v-row v-if="getPost.description" class="font-weight-light" no-gutters>
           <div class="font-weight-bold text-body-2" no-gutters>توضیح پُست</div>
@@ -129,6 +165,7 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 
+import ProductTag from '@/components/product/ProductTag'
 import ProductPriceForm from '@/components/product/ProductPriceForm'
 import ProductEditForm from '@/components/product/ProductEditForm'
 import ProductDiscountForm from '@/components/product/ProductDiscountForm'
@@ -137,6 +174,7 @@ import ProductAttributesForm from '@/components/product/ProductAttributesForm'
 export default {
   name: 'ProductShortcodePage',
   components: {
+    ProductTag,
     ProductPriceForm,
     ProductEditForm,
     ProductDiscountForm,
@@ -186,6 +224,9 @@ export default {
 
     productImageUrl (productImage) {
       return process.env.baseURL + productImage.displayImage
+    },
+    t () {
+      console.log('hello')
     },
     optionsOnClick (formName) {
       this.showDialog = true
@@ -237,40 +278,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.tag {
-  pointer-events: none;
-  position: absolute;
-  -webkit-transform: scale(0);
-  transform: scale(0);
-  -webkit-transform-origin: center top;
-  transform-origin: center top;
-  -webkit-transition-duration: .2s;
-  transition-duration: .2s;
-  -webkit-transition-property: opacity,-webkit-transform;
-  transition-property: transform,opacity;
-  transition-property: transform,opacity,-webkit-transform;
-  -webkit-transition-timing-function: cubic-bezier(.16,1.275,.725,1.255);
-  transition-timing-function: cubic-bezier(.16,1.275,.725,1.255);
-}
-.arrow-up {
-  width: 0;
-  height: 0;
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  border-bottom: 5px solid black;
-  margin-right: 40%;
-}
-.currency {
-  -webkit-text-size-adjust: 100%;
-    direction: rtl;
-    list-style: none;
-    text-align: center;
-    -webkit-box-direction: normal;
-    box-sizing: inherit;
-    outline: none!important;
-    font-size: .540rem;
-    line-height: 21px;
-}
-</style>
