66586988ecfedd72b8e521ff6ec05b4eb3eb74c5
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-18 14:54:02 +0330

    persian price text added under the text field

--- components/product/ProductPriceForm.vue
+++ components/product/ProductPriceForm.vue
@@ -20,10 +20,12 @@
           :rules="priceRules"
           type="number"
           suffix="تومان"
+          prefix="000"
           outlined
           dense
         />
       </v-col>
+      <div v-show="priceText" class="px-8 pb-2 text-caption">{{ priceText }} تومان</div>
 
       <v-card-actions class="px-8 pb-5">
         <v-col cols="12">
@@ -44,6 +46,8 @@
 </template>
 
 <script>
+import Num2persian from 'num2persian'
+
 export default {
   name: 'ProductPriceForm',
   props: {
@@ -60,22 +64,31 @@ export default {
   data () {
     return {
       price: '',
+      priceText: '',
       priceRules: [
         value => !!value || 'قیمت را وارد کنید.',
-        value => value >= 0 || '.قیمت نامعتبر است',
+        value => value >= 5 || 'قیمت نامعتبر است.',
         value => !value.includes('.') || 'قیمت نامعتبر است'
       ]
+    }
+  },
+  watch: {
+    price (newValue) {
+      this.priceText = Num2persian(newValue + '000')
     }
   },
   mounted () {
     if (this.productPrice) {
       this.price = this.productPrice.toString()
+      if (this.price) {
+        this.price = (this.price / 1000).toString()
+      }
     }
   },
   methods: {
     validateAndSubmitForm () {
       if (this.$refs.form.validate()) {
-        this.$emit('submit', Number.parseInt(this.price))
+        this.$emit('submit', Number.parseInt(this.price) * 1000)
       }
     }
   }
