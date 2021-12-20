485a99880a608846b290d94fd817eca38c8064ff
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-21 00:00:12 +0330

    shop preview mock data added

---
+++ mock/shop_preview.ts
@@ -0,0 +1,219 @@
+const shopPreviewData = [
+  {
+    index: 0,
+    id: '2550250387749531538',
+    displayImage: 'media/shop/utabwood/2550250387749531538/display_image.jpg',
+    children: []
+  },
+  {
+    index: 1,
+    id: '2461594798190766919',
+    displayImage: 'media/shop/utabwood/2461594798190766919/display_image.jpg',
+    children: [
+      {
+        index: 0,
+        id: '2461594794567010945',
+        displayImage: 'media/shop/utabwood/2461594798190766919/image2.jpg',
+        parent: '2461594798190766919'
+      },
+      {
+        index: 1,
+        id: '2461594794575369257',
+        displayImage: 'media/shop/utabwood/2461594798190766919/image3.jpg',
+        parent: '2461594798190766919'
+      }
+    ]
+  },
+  {
+    index: 2,
+    id: '2460999396328515685',
+    displayImage: 'media/shop/utabwood/2460999396328515685/display_image.jpg',
+    children: [
+      {
+        index: 0,
+        id: '2460999392612385661',
+        displayImage: 'media/shop/utabwood/2460999396328515685/image2.jpg',
+        parent: '2460999396328515685'
+      }
+    ]
+  },
+  {
+    index: 3,
+    id: '2081862955232131478',
+    displayImage: 'media/shop/utabwood/2081862955232131478/display_image.jpg',
+    children: []
+  },
+  {
+    index: 4,
+    id: '1945319255251774154',
+    displayImage: 'media/shop/utabwood/1945319255251774154/display_image.jpg',
+    children: []
+  },
+  {
+    index: 5,
+    id: '1944949353307282019',
+    displayImage: 'media/shop/utabwood/1944949353307282019/display_image.jpg',
+    children: []
+  },
+  {
+    index: 6,
+    id: '1943356604381291815',
+    displayImage: 'media/shop/utabwood/1943356604381291815/display_image.jpg',
+    children: [
+      {
+        index: 0,
+        id: '1943355425874027855',
+        displayImage: 'media/shop/utabwood/1943356604381291815/image2.jpg',
+        parent: '1943356604381291815'
+      },
+      {
+        index: 1,
+        id: '1943355425832207133',
+        displayImage: 'media/shop/utabwood/1943356604381291815/image3.jpg',
+        parent: '1943356604381291815'
+      },
+      {
+        index: 2,
+        id: '1943355428122173074',
+        displayImage: 'media/shop/utabwood/1943356604381291815/image4.jpg',
+        parent: '1943356604381291815'
+      },
+      {
+        index: 3,
+        id: '1943355438649886974',
+        displayImage: 'media/shop/utabwood/1943356604381291815/image5.jpg',
+        parent: '1943356604381291815'
+      },
+      {
+        index: 4,
+        id: '1943355441233589804',
+        displayImage: 'media/shop/utabwood/1943356604381291815/image6.jpg',
+        parent: '1943356604381291815'
+      }
+    ]
+  },
+  {
+    index: 7,
+    id: '1942733576584813188',
+    displayImage: 'media/shop/utabwood/1942733576584813188/display_image.jpg',
+    children: [
+      {
+        index: 0,
+        id: '1942732338912677306',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image2.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 1,
+        id: '1942732340431126832',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image3.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 2,
+        id: '1942732340313557984',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image4.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 3,
+        id: '1942732340959612579',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image5.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 4,
+        id: '1942732340313547090',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image6.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 5,
+        id: '1942732340741299344',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image7.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 6,
+        id: '1942732355832429843',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image8.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 7,
+        id: '1942732354507198412',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image9.jpg',
+        parent: '1942733576584813188'
+      },
+      {
+        index: 8,
+        id: '1942732355723331179',
+        displayImage: 'media/shop/utabwood/1942733576584813188/image10.jpg',
+        parent: '1942733576584813188'
+      }
+    ]
+  },
+  {
+    index: 8,
+    id: '1942187651814618797',
+    displayImage: 'media/shop/utabwood/1942187651814618797/display_image.jpg',
+    children: [
+      {
+        index: 0,
+        id: '1942186129659596088',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image2.jpg',
+        parent: '1942187651814618797'
+      },
+      {
+        index: 1,
+        id: '1942186131060491981',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image3.jpg',
+        parent: '1942187651814618797'
+      },
+      {
+        index: 2,
+        id: '1942186132377680884',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image4.jpg',
+        parent: '1942187651814618797'
+      },
+      {
+        index: 3,
+        id: '1942186144222209583',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image5.jpg',
+        parent: '1942187651814618797'
+      },
+      {
+        index: 4,
+        id: '1942186144238977558',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image6.jpg',
+        parent: '1942187651814618797'
+      },
+      {
+        index: 5,
+        id: '1942186144876474721',
+        displayImage: 'media/shop/utabwood/1942187651814618797/image7.jpg',
+        parent: '1942187651814618797'
+      }
+    ]
+  },
+  {
+    index: 9,
+    id: '1941472893213680450',
+    displayImage: 'media/shop/utabwood/1941472893213680450/display_image.jpg',
+    children: []
+  },
+  {
+    index: 10,
+    id: '1941458700469068956',
+    displayImage: 'media/shop/utabwood/1941458700469068956/display_image.jpg',
+    children: []
+  },
+  {
+    index: 11,
+    id: '1941452659329130711',
+    displayImage: 'media/shop/utabwood/1941452659329130711/display_image.jpg',
+    children: []
+  }
+]
+
+export default shopPreviewData
