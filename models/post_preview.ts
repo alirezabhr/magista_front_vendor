991b8c0137317a17d6ebfa8473cf443caa4c40d2
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-20 20:31:28 +0330

    PostChild model added in children field in PostPreview

--- models/post_preview.ts
+++ models/post_preview.ts
@@ -1,13 +1,29 @@
-class PostPreview {
-    index: number
-    id: number
-    thumbnailSrc: string
+class PostChild {
+  index: number
+  id: string
+  displayImage: string
+  parent: string
 
-    constructor(index: number, id: number, thumbnailSrc: string) {
-        this.index = index
-        this.id = id
-        this.thumbnailSrc = thumbnailSrc
-    }
+  constructor (index: number, id: string, displayImage: string, parent: string) {
+    this.index = index
+    this.id = id
+    this.displayImage = displayImage
+    this.parent = parent
+  }
+}
+
+class PostPreview {
+  index: number
+  id: string
+  displayImage: string
+  children: PostChild[]
+
+  constructor (index: number, id: string, displayImage: string, children: PostChild[]) {
+    this.index = index
+    this.id = id
+    this.displayImage = displayImage
+    this.children = children
+  }
 }
 
 export default PostPreview
