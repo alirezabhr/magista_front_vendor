bafa3f5e6ee5ea6fdc968a47155690279c4d14ba
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-31 14:09:20 +0330

    chart plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/initial_stores.client',
-    '@/plugins/dependencyInjection'
+    '@/plugins/dependencyInjection',
+    '@/plugins/chart.client'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
