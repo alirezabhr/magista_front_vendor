afbe1a63279fa70154d0bf204801e7753467799b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-03 16:58:02 +0330

    BankCreditForm completed and methods implemented to create new BankCredit with sheba number

--- components/BankCreditForm.vue
+++ components/BankCreditForm.vue
@@ -14,27 +14,35 @@
       </v-card-title>
 
       <v-col class="py-0 px-8">
-        <v-row v-show="finalPrice" no-gutters class="pb-2 text-body1" align="center">
-          <div class="text-decoration-line-through font-weight-medium">{{ productPrice }}</div>
-          <v-icon class="px-2">mdi-chevron-triple-left</v-icon>
-          <div class="font-weight-bold primary--text">{{ finalPrice }}</div>
-        </v-row>
         <v-text-field
-          v-model="discountPercent"
-          label="درصد تخفیف"
-          :rules="discountPercentRules"
-          type="number"
-          prefix="%"
+          v-model="shebaData.sheba"
+          label="شماره شبا"
+          hint="مثال: IRxxxxxxxxxxxxxxxxxxxxxx"
+          dir="ltr"
+          :rules="shebaRules"
           outlined
           dense
         />
-        <v-textarea
-          v-model="discountDescription"
-          label="توضیح (اختیاری)"
-          :rules="discountDescriptionRules"
-          outlined
-          dense
-        />
+        <v-row no-gutters>
+          <v-col class="pa-0 ml-1">
+            <v-text-field
+              v-model="shebaData.firstName"
+              label="نام"
+              :rules="firstNameRules"
+              outlined
+              dense
+            />
+          </v-col>
+          <v-col class="pa-0 mr-1">
+            <v-text-field
+              v-model="shebaData.lastName"
+              label="نام خانوادگی"
+              :rules="lastNameRules"
+              outlined
+              dense
+            />
+          </v-col>
+        </v-row>
       </v-col>
       <v-card-actions class="px-8 pb-5 pt-4">
         <v-col cols="12">
@@ -55,7 +63,46 @@
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex'
 
+export default {
+  name: 'BankCrediForm',
+  data () {
+    return {
+      isSubmittingForm: false,
+      shebaData: {
+        sheba: '',
+        firstName: '',
+        lastName: ''
+      },
+      shebaRules: [
+        value => !!value || 'شماره شبا الزامی می‌باشد.',
+        value => value.length === 26 || 'شماره شبا دارای دو حرف IR و 24 رقم می‌باشد.',
+        value => value.startsWith('IR') || 'شماره شبا باید با IR شروع شود.',
+        value => !value.includes('.') || 'شماره شبا نامعتبر است.'
+      ],
+      firstNameRules: [
+        value => !!value || 'نام دارنده حساب الزامی می‌باشد.',
+        value => value.length < 40 || 'نام بسیار طولانی است.'
+      ],
+      lastNameRules: [
+        value => !!value || 'نام خانوادگی دارنده حساب الزامی می‌باشد.',
+        value => value.length < 40 || 'نام خانوادگی بسیار طولانی است.'
+      ]
+    }
+  },
+  methods: {
+    ...mapActions('shop', ['addBankCredit']),
+
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.isSubmittingForm = true
+        this.addBankCredit(this.shebaData).then(() => {
+          this.isSubmittingForm = false
+          this.$emit('close')
+        })
+      }
+    }
+  }
 }
 </script>
