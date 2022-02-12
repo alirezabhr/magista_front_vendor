0b286476acd3d121d082821cb46758b2b96b3cf8
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-12 21:16:20 +0330

    profile shipping page created to change delivery methods and costs

---
+++ pages/profile/shipping/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="indigo lighten-5" style="height: 100%">
+    <ShippingForm :shop-city="shopCity" @submit="submit" />
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ShippingForm from '@/components/ShippingForm.vue'
+
+export default {
+  name: 'ProfileShippingPage',
+  components: {
+    ShippingForm
+  },
+  computed: {
+    ...mapGetters('shop', ['getCurrentShop']),
+
+    shopCity () {
+      return this.getCurrentShop.city
+    }
+  },
+  methods: {
+    submit (payload) {
+      console.log(payload)
+    }
+  }
+}
+</script>
