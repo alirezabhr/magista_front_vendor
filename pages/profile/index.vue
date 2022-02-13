5b6ba39850133606b48efe1da8ad40fc53dee542
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-13 21:55:30 +0330

    delivery card added in profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -19,6 +19,39 @@
     <v-card class="pb-2 my-4 mx-4">
       <v-card-title class="font-weight-bold text-h5 primary--text pb-3">
         <v-row justify="center" no-gutters>
+          روش و هزینه ارسال
+        </v-row>
+      </v-card-title>
+      <v-card-text class="py-1">
+        ارسال به:
+        <span v-if="getCurrentShopDeliveryDestination" class="font-weight-bold">
+          {{ getCurrentShopDeliveryDestination }}
+        </span>
+      </v-card-text>
+      <v-card-text class="py-1">
+        روش‌های ارسال:
+        <v-col v-if="getCurrentShopDelivery" class="font-weight-bold py-0">
+          <div v-if="getCurrentShopDelivery.hasNationalPost">
+            <v-icon>mdi-circle-medium</v-icon>
+            پست پیشتاز
+          </div>
+          <div v-if="getCurrentShopDelivery.hasOnlineDelivery">
+            <v-icon>mdi-circle-medium</v-icon>
+            پیک آنلاین
+          </div>
+        </v-col>
+      </v-card-text>
+      <v-card-actions>
+        <v-col cols="6" class="pa-0 mx-auto">
+          <v-btn width="100%" outlined class="red--text font-weight-bold" @click.prevent="$router.push('/profile/shipping')">
+            تغییر
+          </v-btn>
+        </v-col>
+      </v-card-actions>
+    </v-card>
+    <v-card class="pb-2 my-4 mx-4">
+      <v-card-title class="font-weight-bold text-h5 primary--text pb-3">
+        <v-row justify="center" no-gutters>
           کیف پول
         </v-row>
       </v-card-title>
@@ -27,14 +60,23 @@
       </v-card-text>
       <v-card-actions>
         <v-col cols="6" class="pa-0 mx-auto">
-          <v-btn width="100%" class="yellow darken-1 white--text font-weight-bold" @click.prevent="$router.push('/profile/withdraw')">
+          <v-btn width="100%" dark class="yellow darken-1 font-weight-bold" @click.prevent="$router.push('/profile/withdraw')">
             تسویه
           </v-btn>
         </v-col>
       </v-card-actions>
     </v-card>
     <v-row no-gutters>
-      <v-btn class="mx-auto font-weight-bold text-body-1" outlined color="red" text @click.prevent="userLogout">خروج</v-btn>
+      <v-btn
+        text
+        outlined
+        width="80%"
+        color="red"
+        class="mx-auto font-weight-bold text-body-1"
+        @click.prevent="userLogout"
+      >
+        خروج
+      </v-btn>
     </v-row>
   </v-container>
 </template>
@@ -46,7 +88,18 @@ export default {
   name: 'ProfilePage',
   layout: 'panel',
   computed: {
-    ...mapGetters('shop', ['getCurrentShop'])
+    ...mapGetters('shop', ['getCurrentShop', 'getCurrentShopDelivery']),
+
+    getCurrentShopDeliveryDestination () {
+      if (this.getCurrentShopDelivery) {
+        if (this.getCurrentShopDelivery.sendEverywhere) {
+          return 'سراسر ایران'
+        } else {
+          return this.getCurrentShop.city
+        }
+      }
+      return null
+    }
   },
   methods: {
     ...mapActions('auth', ['userLogout'])
