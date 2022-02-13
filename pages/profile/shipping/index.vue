00d8449e6e5aa3958283947d6dd7065f4e076014
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-02-13 21:42:31 +0330

    shipping page created

--- pages/profile/shipping/index.vue
+++ pages/profile/shipping/index.vue
@@ -1,28 +1,57 @@
 <template>
   <v-container class="indigo lighten-5" style="height: 100%">
-    <ShippingForm :shop-city="shopCity" @submit="submit" />
+    <v-snackbar
+      v-model="showSnackbar"
+      elevation="24"
+      :color="snackbarColor"
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
+    <ShippingForm
+      :shop-city="getCurrentShop.city"
+      :shop-delivery-data="getCurrentShopDelivery"
+      :is-submitting="isSubmittingShippingForm"
+      @submit="submit"
+    />
   </v-container>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import ShippingForm from '@/components/ShippingForm.vue'
+import { mapGetters, mapActions } from 'vuex'
+import ShippingForm from '~/components/shipping/ShippingForm.vue'
 
 export default {
   name: 'ProfileShippingPage',
   components: {
     ShippingForm
   },
-  computed: {
-    ...mapGetters('shop', ['getCurrentShop']),
-
-    shopCity () {
-      return this.getCurrentShop.city
+  data () {
+    return {
+      showSnackbar: false,
+      snackbarMessage: '',
+      snackbarColor: 'red',
+      isSubmittingShippingForm: false
     }
   },
+  computed: {
+    ...mapGetters('shop', ['getCurrentShop', 'getCurrentShopDelivery'])
+  },
   methods: {
+    ...mapActions('shop', ['updateShopShipping']),
+
     submit (payload) {
-      console.log(payload)
+      this.isSubmittingShippingForm = true
+      this.updateShopShipping(payload).then(() => {
+        this.snackbarColor = 'green'
+        this.snackbarMessage = 'تغییرات ذخیره شد.'
+        this.showSnackbar = true
+        this.isSubmittingShippingForm = false
+      }).catch(() => {
+        this.isSubmittingShippingForm = false
+        this.snackbarColor = 'red'
+        this.snackbarMessage = 'ثبت با خطا مواجه شد.'
+        this.showSnackbar = true
+      })
     }
   }
 }
